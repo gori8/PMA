ea3bbf40ed3400cf1de64ecbf95cc17c4418d920
shekrba <kickapoo889@gmail.com>
2020-04-25 02:48:56 +0200

    Friend list

--- Sportly/app/src/main/java/rs/ac/uns/ftn/sportly/ui/friends/FriendsFragment.java
+++ Sportly/app/src/main/java/rs/ac/uns/ftn/sportly/ui/friends/FriendsFragment.java
@@ -1,39 +1,48 @@
 package rs.ac.uns.ftn.sportly.ui.friends;
 
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import rs.ac.uns.ftn.sportly.R;
-import rs.ac.uns.ftn.sportly.ui.map.MapViewModel;
 
 public class FriendsFragment extends Fragment {
 
-    private FriendsViewModel friendsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        friendsViewModel =
-                ViewModelProviders.of(this).get(FriendsViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_friends, container, false);
-        final TextView textView = root.findViewById(R.id.text_friends);
-        friendsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        List<String> names = new ArrayList<>();
+        names.add("Milan Škrbić");
+        names.add("Igor Antolović");
+        names.add("Stevan Vulić");
+
+        List<Integer> images = new ArrayList<>();
+        images.add(R.drawable.milan_skrbic);
+        images.add(R.drawable.igor_antolovic);
+        images.add(R.drawable.stevan_vulic);
+
+        FriendsAdapter adapter = new FriendsAdapter(getContext(), names, images);
+
+        ListView listView = (ListView) getActivity().findViewById(R.id.friends_list);
+        listView.setAdapter(adapter);
+    }
 }
