cf09658b7e95b6f7249819e19fa6d534fe887685
shekrba <kickapoo889@gmail.com>
2020-05-02 19:53:12 +0200

    Event overview

---
+++ Sportly/app/src/main/java/rs/ac/uns/ftn/sportly/ui/event/EventActivity.java
@@ -0,0 +1,16 @@
+package rs.ac.uns.ftn.sportly.ui.event;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import rs.ac.uns.ftn.sportly.R;
+
+public class EventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+    }
+}
