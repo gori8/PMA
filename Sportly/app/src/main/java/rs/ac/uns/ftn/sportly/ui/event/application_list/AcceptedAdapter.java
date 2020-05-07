ad905fd60425ca8ac255f87c6ecccc23fc632d44
shekrba <kickapoo889@gmail.com>
2020-05-07 23:34:40 +0200

    Application List Accepted done

---
+++ Sportly/app/src/main/java/rs/ac/uns/ftn/sportly/ui/event/application_list/AcceptedAdapter.java
@@ -0,0 +1,44 @@
+package rs.ac.uns.ftn.sportly.ui.event.application_list;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+import rs.ac.uns.ftn.sportly.R;
+
+public class AcceptedAdapter extends ArrayAdapter<String> {
+
+    private Context context;
+    private List<String> names;
+    private List<Integer> images;
+
+    AcceptedAdapter (Context c, List<String> n, List<Integer> i){
+        super(c, R.layout.queue_item, R.id.name, n);
+        this.context = c;
+        this.names = n;
+        this.images = i;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = layoutInflater.inflate(R.layout.accepted_item, parent, false);
+        ImageView imageView = row.findViewById(R.id.image);
+        TextView textViewName = row.findViewById(R.id.name);
+
+        imageView.setImageResource(images.get(position));
+        textViewName.setText(names.get(position));
+
+        return row;
+    }
+}
