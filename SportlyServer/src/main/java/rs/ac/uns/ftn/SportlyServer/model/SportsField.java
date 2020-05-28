cd82c92933a43ac6aae420cc9d6e52b4354bb542
gori8 <goriantolovic@gmail.com>
2020-05-28 19:42:14 +0200

    Added model classes

---
+++ SportlyServer/src/main/java/rs/ac/uns/ftn/SportlyServer/model/SportsField.java
@@ -0,0 +1,31 @@
+package rs.ac.uns.ftn.SportlyServer.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class SportsField {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private float latitude;
+
+    private float longitude;
+
+
+}
