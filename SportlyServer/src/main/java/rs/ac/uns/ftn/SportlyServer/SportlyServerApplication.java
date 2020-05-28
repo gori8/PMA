77b3f4567b6f4b5b7a9f55feeba7d04bd009f081
gori8 <goriantolovic@gmail.com>
2020-05-28 19:19:27 +0200

    Server app added

---
+++ SportlyServer/src/main/java/rs/ac/uns/ftn/SportlyServer/SportlyServerApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.SportlyServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportlyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportlyServerApplication.class, args);
+	}
+
+}
---
+++ SportlyServer/src/test/java/rs/ac/uns/ftn/SportlyServer/SportlyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.SportlyServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportlyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
