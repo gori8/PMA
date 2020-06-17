f88808fb337f581ecb3a817eb799961d575fd993
gori8 <goriantolovic@gmail.com>
2020-06-17 04:39:07 +0200

    Notifications system implmented with firebase and spring

--- SportlyServer/src/main/java/rs/ac/uns/ftn/SportlyServer/service/SyncService.java
+++
@@ -1,8 +0,0 @@
-package rs.ac.uns.ftn.SportlyServer.service;
-
-import rs.ac.uns.ftn.SportlyServer.dto.SyncDataDTO;
-
-public interface SyncService {
-
-    public SyncDataDTO getSyncData(String username);
-}
