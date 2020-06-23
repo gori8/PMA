39345ea4b78994d4d6a8281a8c1cdf14564b4721
Vul4BP <44211967+Vul4BP@users.noreply.github.com>
2020-06-24 00:35:56 +0200

    Added crud for Events

---
+++ SportlyServer/src/main/java/rs/ac/uns/ftn/SportlyServer/repository/EventRepository.java
@@ -0,0 +1,17 @@
+package rs.ac.uns.ftn.SportlyServer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import rs.ac.uns.ftn.SportlyServer.model.Event;
+import rs.ac.uns.ftn.SportlyServer.model.User;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+    Event getById(Long id);
+    List<Event> findByDateFromAfterAndDateToBefore(Date dateFrom , Date dateTo);
+    List<Event> findByNameContains(String name);
+}
