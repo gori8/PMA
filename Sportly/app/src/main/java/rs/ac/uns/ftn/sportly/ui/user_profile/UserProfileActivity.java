bf4fe891cbd24cc4b46a2150894f81264c1d69f3
shekrba <kickapoo889@gmail.com>
2020-07-02 01:25:51 +0200

    provera

--- Sportly/app/src/main/java/rs/ac/uns/ftn/sportly/ui/user_profile/UserProfileActivity.java
+++ Sportly/app/src/main/java/rs/ac/uns/ftn/sportly/ui/user_profile/UserProfileActivity.java
@@ -1,6 +1,8 @@
 package rs.ac.uns.ftn.sportly.ui.user_profile;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -9,20 +11,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import rs.ac.uns.ftn.sportly.R;
+import rs.ac.uns.ftn.sportly.dto.FriendshipDTO;
+import rs.ac.uns.ftn.sportly.dto.PersonToRateDTO;
+import rs.ac.uns.ftn.sportly.service.SportlyServerServiceUtils;
 
 public class UserProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
         Intent myIntent = getIntent();
-        String name = myIntent.getStringExtra("name");
-        String surname = myIntent.getStringExtra("surname");
-        String username = myIntent.getStringExtra("username");
-        String email = myIntent.getStringExtra("email");
-        int photoUrl = myIntent.getIntExtra("photoUrl",0);
+        Long userId = myIntent.getLongExtra("id",-1L);
+
+        Call<FriendshipDTO> call = SportlyServerServiceUtils.sportlyServerService.addFriend(authHeader,request);
+
+        call.enqueue(new Callback<FriendshipDTO>() {
+            @Override
+            public void onResponse(Call<FriendshipDTO> call, Response<FriendshipDTO> response) {
+                if (response.code() == 200){
+
+                    Log.i("ADD FRIEND", "CALL TO SERVER SUCCESSFUL");
+
+                    loadingCircle.setVisibility(View.GONE);
+                    removeButton.setVisibility(View.VISIBLE);
+
+                }else{
+                    Log.i("ADD FRIEND", "CALL TO SERVER RESPONSE CODE: "+response.code());
+                    loadingCircle.setVisibility(View.GONE);
+                    addButton.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<FriendshipDTO> call, Throwable t) {
+                Log.i("REZ", t.getMessage() != null?t.getMessage():"error");
+                Log.i("ADD FRIEND", "CALL TO SERVER FAILED");
+                loadingCircle.setVisibility(View.GONE);
+                addButton.setVisibility(View.VISIBLE);
+            }
+        });
 
         TextView nameSurnameUsernameTV = findViewById(R.id.user_profile_name_surname_username);
         String nameSurnameUsernameText = name + " " + surname + " (" + username + ") ";
@@ -35,22 +73,10 @@ public class UserProfileActivity extends AppCompatActivity {
         img.setImageResource(photoUrl);
 
         List<String> names = new ArrayList<>();
-        names.add("Milan Skrbic");
-        names.add("Igor Antolovic");
 
-        List<Integer> images = new ArrayList<>();
-        images.add(R.drawable.milan_skrbic);
-        images.add(R.drawable.igor_antolovic);
+        List<PersonToRateDTO> ratings = new ArrayList<>();
 
-        List<String> descriptions = new ArrayList<>();
-        descriptions.add("Odlican igrac. Odlicna saradnja i dogovor.");
-        descriptions.add("Nije lose moze bolje.");
-
-        List<Float> ratings = new ArrayList<>();
-        ratings.add(4f);
-        ratings.add(3f);
-
-        UserProfileAdapter adapter = new UserProfileAdapter(this, names, images, descriptions, ratings);
+        UserProfileAdapter adapter = new UserProfileAdapter(this, ratings, names);
 
         ListView listView = (ListView) findViewById(R.id.user_profile_ratings);
         listView.setAdapter(adapter);
