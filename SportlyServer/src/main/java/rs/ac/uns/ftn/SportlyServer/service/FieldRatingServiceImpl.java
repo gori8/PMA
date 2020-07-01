82ba1d2684152b40ec3d08f7a17526e29427baa6
shekrba <kickapoo889@gmail.com>
2020-07-02 00:51:25 +0200

    Postavljanje ocene i komentara implementirano

--- SportlyServer/src/main/java/rs/ac/uns/ftn/SportlyServer/service/FieldRatingServiceImpl.java
+++ SportlyServer/src/main/java/rs/ac/uns/ftn/SportlyServer/service/FieldRatingServiceImpl.java
@@ -2,12 +2,16 @@ package rs.ac.uns.ftn.SportlyServer.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import rs.ac.uns.ftn.SportlyServer.dto.BundleRatingDTO;
 import rs.ac.uns.ftn.SportlyServer.dto.FieldRatingDTO;
+import rs.ac.uns.ftn.SportlyServer.dto.PersonToRateDTO;
 import rs.ac.uns.ftn.SportlyServer.model.FieldRating;
 import rs.ac.uns.ftn.SportlyServer.model.SportsField;
 import rs.ac.uns.ftn.SportlyServer.model.User;
+import rs.ac.uns.ftn.SportlyServer.model.UserRating;
 import rs.ac.uns.ftn.SportlyServer.repository.FieldRatingRepository;
 import rs.ac.uns.ftn.SportlyServer.repository.SportsFieldRepository;
+import rs.ac.uns.ftn.SportlyServer.repository.UserRatingRepository;
 import rs.ac.uns.ftn.SportlyServer.repository.UserRepository;
 
 import java.util.ArrayList;
@@ -24,6 +28,9 @@ public class FieldRatingServiceImpl implements FieldRatingService {
 
     @Autowired
     FieldRatingRepository fieldRatingRepository;
+
+    @Autowired
+    UserRatingRepository userRatingRepository;
 
     @Override
     public FieldRatingDTO getRating(Long id) {
@@ -127,5 +134,65 @@ public class FieldRatingServiceImpl implements FieldRatingService {
         sportsField.setRating(avg);
         sportsFieldRepository.save(sportsField);
         return sportsField;
+    }
+
+    @Override
+    public Long createBundleRating(BundleRatingDTO bundle, String reqEmail){
+
+        User creator = userRepository.findByEmail(reqEmail);
+        if(creator == null)
+            return null;
+
+        SportsField sportsField = sportsFieldRepository.getById(bundle.getSportsFieldId());
+        if(sportsField == null)
+            return null;
+
+        for (PersonToRateDTO person : bundle.getPeople()) {
+
+            if(person.getRating() == 0.0){
+                continue;
+            }
+
+            User user = userRepository.getOne(person.getId());
+
+            if(user == null)
+                return null;
+
+            UserRating userRating = new UserRating();
+            userRating.setUserCreator(creator);
+            userRating.setUser(user);
+            userRating.setComment(person.getComment());
+            userRating.setValue((short) Math.round(person.getRating()));
+
+            userRating = userRatingRepository.save(userRating);
+
+            creator.getRatedUsers().add(userRating);
+            userRepository.save(creator);
+
+            user.getUserRatings().add(userRating);
+            userRepository.save(user);
+        }
+
+        if(bundle.getSportsFieldRating() == 0.0){
+            return -1L;
+        }
+
+        FieldRating fieldRating = new FieldRating();
+        fieldRating.setComment(bundle.getSportsFieldComment());
+        fieldRating.setValue((short) Math.round(bundle.getSportsFieldRating()));
+        fieldRating.setField(sportsField);
+        fieldRating.setUserCreator(creator);
+
+        fieldRating = fieldRatingRepository.save(fieldRating);
+
+        creator.getRatedFields().add(fieldRating);
+        userRepository.save(creator);
+
+        sportsField.getFieldRatings().add(fieldRating);
+        sportsFieldRepository.save(sportsField);
+
+        UpdateAverageFieldRating(sportsField);
+
+        return fieldRating.getId();
     }
 }
