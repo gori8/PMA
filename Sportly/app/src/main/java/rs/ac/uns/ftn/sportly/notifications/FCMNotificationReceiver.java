b06fdcc9e62750b032765361923b013f2d6c4608
gori8 <goriantolovic@gmail.com>
2020-07-02 01:03:12 +0200

    Rating system full flow implemented

--- Sportly/app/src/main/java/rs/ac/uns/ftn/sportly/notifications/FCMNotificationReceiver.java
+++ Sportly/app/src/main/java/rs/ac/uns/ftn/sportly/notifications/FCMNotificationReceiver.java
@@ -12,13 +12,18 @@ import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
 
 import androidx.core.app.NotificationCompat;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import rs.ac.uns.ftn.sportly.MainActivity;
 import rs.ac.uns.ftn.sportly.R;
+import rs.ac.uns.ftn.sportly.ui.rating.RatingActivity;
 
 
 public class FCMNotificationReceiver extends BroadcastReceiver {
@@ -29,16 +34,31 @@ public class FCMNotificationReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 
 		if(intent.getAction().equals(MainActivity.NOTIFICATION_INTENT)){
-			sendNotification(context,intent.getStringExtra("title"),intent.getStringExtra("body"));
+			HashMap<String,String> data = new HashMap<>();
+			for(String key : intent.getExtras().keySet()){
+				if(key.equals("notificationType") || key.equals("message") || key.equals("title")){
+					continue;
+				}
+				data.put(key,intent.getStringExtra(key));
+			}
+			sendNotification(context,intent.getStringExtra("title"),intent.getStringExtra("message"), intent.getStringExtra("notificationType"),data);
 		}
 
 	}
 
-	private void sendNotification(Context context, String title, String messageBody) {
-		Intent intent = new Intent(context, MainActivity.class);
-		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0 /* Request code */, intent,
-				PendingIntent.FLAG_ONE_SHOT);
+	private void sendNotification(Context context, String title, String messageBody, String notificationType, HashMap<String,String> data) {
+		PendingIntent pendingIntent = null;
+		if(notificationType.equals("RATING_REQUEST")) {
+			Intent intent = new Intent(context, RatingActivity.class);
+			Bundle bundleData = new Bundle();
+			for (Map.Entry<String, String> entry : data.entrySet()) {
+				bundleData.putString(entry.getKey(), entry.getValue());
+			}
+			intent.putExtras(bundleData);
+			pendingIntent = PendingIntent.getActivity(context, 0, intent,
+					PendingIntent.FLAG_ONE_SHOT);
+		}
+
 
 		String channelId = context.getString(R.string.default_notification_channel_id);
 		Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
